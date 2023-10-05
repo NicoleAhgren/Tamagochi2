5b5c5bca58da73659a52023ddb0db4ebd24d5179
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2023-10-05 14:06:09 +0200

    hej

---
+++ Tamagochi2/Tamagochi.cs
@@ -0,0 +1,17 @@
+public class Tamagochi
+{
+    int hunger;
+    int boredom;
+    List<string> words;
+    bool isAlive = true;
+
+    Random generator  = new Random();
+
+    public string name;
+
+    public void Printstats()
+    {
+        
+    }
+
+}
